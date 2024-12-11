45d30f64087146e487329e96233d58fefdf6b057
feizhai <74600625+1731010016@users.noreply.github.com>
2024-12-12 00:43:20 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -18,20 +18,21 @@ int main() {
 		return -1;
 	}
 
-	sockaddr_in serverAddr;
+	sockaddr_in serverAddr = { 0 };
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(8082);  // 目标端口
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // 目标 IP 地址
+	serverAddr.sin_port = htons(8182);  // 目标端口
+	serverAddr.sin_addr.s_addr = inet_addr("0.0.0.0");  // 目标 IP 地址
+	bind(listen_socket, (sockaddr*)&serverAddr,1);
 
-	int code = connect(listen_socket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+	//int code = connect(listen_socket, (sockaddr*)&serverAddr, sizeof(serverAddr));
 
-	if (code == SOCKET_ERROR) {
-		std::cout << GetLastError();
-		std::cerr << "Connection failed!" << std::endl;
-		closesocket(listen_socket);
-		WSACleanup();
-		return -1;
-	}
+	//if (code == SOCKET_ERROR) {
+	//	std::cout << GetLastError();
+	//	std::cerr << "Connection failed!" << std::endl;
+	//	closesocket(listen_socket);
+	//	WSACleanup();
+	//	return -1;
+	//}
 	//std::this_thread::sleep_for(std::chrono::seconds(10));
 
 
